cbfb7a5a85bdc83908a00dc31b91fca3ffcd9771
Jo Grimstad <jo@literacyapp.org>
2016-11-21 10:09:29 +0300

    #26 Changes after code review

--- app/src/main/java/org/literacyapp/appstore/service/ApplicationSynchronizationService.java
+++ app/src/main/java/org/literacyapp/appstore/service/ApplicationSynchronizationService.java
@@ -5,19 +5,12 @@ import android.app.job.JobService;
 import android.util.Log;
 
 import org.literacyapp.appstore.task.DownloadApplicationsAsyncTask;
-import org.literacyapp.appstore.util.ConnectivityHelper;
 
 public class ApplicationSynchronizationService extends JobService {
 
     @Override
     public boolean onStartJob(JobParameters jobParameters) {
         Log.i(getClass().getName(), "onStartJob");
-
-        boolean isWifiEnabled = ConnectivityHelper.isWifiEnabled(getApplicationContext());
-        Log.i(getClass().getName(), "isWifiEnabled: " + isWifiEnabled);
-
-        boolean isWifiConnected = ConnectivityHelper.isWifiConnected(getApplicationContext());
-        Log.i(getClass().getName(), "isWifiConnected: " + isWifiConnected);
 
         // Start processing work
         new DownloadApplicationsAsyncTask(getApplicationContext()).execute();
