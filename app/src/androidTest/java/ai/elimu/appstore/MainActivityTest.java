85263146ebd461f29e5e2c7a571ce14c92269363
Jo Grimstad <jo.grimstad@elimu.ai>
2017-08-19 21:05:11 +0200

    Added support for Espresso UI tests

---
+++ app/src/androidTest/java/ai/elimu/appstore/MainActivityTest.java
@@ -0,0 +1,35 @@
+package ai.elimu.appstore;
+
+import android.Manifest;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.rule.GrantPermissionRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule grantPermissionRule = GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+    @Test
+    public void testAskForWritePermission() {
+        onView(withId(R.id.textViewLicenseQuestion)).check(matches(withText(R.string.do_you_have_a_license_number)));
+    }
+}
--- app/src/main/java/ai/elimu/appstore/LicenseNumberActivity.java
+++ app/src/main/java/ai/elimu/appstore/LicenseNumberActivity.java
@@ -1,27 +1,90 @@
 package ai.elimu.appstore;
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class LicenseNumberActivity extends AppCompatActivity {
 
     public static final String PREF_LICENSE_EMAIL = "pref_license_email";
     public static final String PREF_LICENSE_NUMBER = "pref_license_number";
 
+    private EditText editTextLicenseEmail;
+
+    private EditText editTextLicenseNumber;
+
+    private Button buttonLicenseNumber;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i(getClass().getName(), "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_license_number);
+
+        editTextLicenseEmail = (EditText) findViewById(R.id.editTextLicenseEmail);
+        editTextLicenseNumber = (EditText) findViewById(R.id.editTextLicenseNumber);
+        buttonLicenseNumber = (Button) findViewById(R.id.buttonLicenseNumber);
     }
 
     @Override
     protected void onStart() {
         Log.i(getClass().getName(), "onStart");
         super.onStart();
+
+        editTextLicenseEmail.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.i(getClass().getName(), "editTextLicenseEmail onKey");
+
+                updateSubmitButton();
+
+                return false;
+            }
+        });
+
+        editTextLicenseNumber.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.i(getClass().getName(), "editTextLicenseNumber onKey");
+
+                updateSubmitButton();
+
+                return false;
+            }
+        });
+
+        buttonLicenseNumber.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(getClass().getName(), "onClick");
+
+                String licenseEmail = editTextLicenseEmail.getText().toString();
+                String licenseNumber = editTextLicenseNumber.getText().toString();
+                if (!TextUtils.isEmpty(licenseEmail) && !TextUtils.isEmpty(licenseNumber)) {
+                    // TODO: submit to REST API for validation
+                    // TODO: if invalid license number, display error message
+                    // TODO: if valid license number, store e-mail and license number in shared preferences
+                    // TODO: if valid license number, fetch and store locale of app collection, and redirect to download activity
+                }
+            }
+        });
+    }
+
+    private void updateSubmitButton() {
+        if (TextUtils.isEmpty(editTextLicenseEmail.getText().toString())
+                || TextUtils.isEmpty(editTextLicenseNumber.getText().toString())) {
+            buttonLicenseNumber.setEnabled(false);
+        } else {
+            buttonLicenseNumber.setEnabled(true);
+        }
     }
 }
