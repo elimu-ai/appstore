02dd427d198b7ec0154a732f24c69d89fec37122
Jo Grimstad <jo.grimstad@elimu.ai>
2017-11-19 23:51:12 +0100

    #75 Submit License details to REST API when pressing the "Save" button

--- app/src/main/java/ai/elimu/appstore/onboarding/LicenseNumberActivity.java
+++ app/src/main/java/ai/elimu/appstore/onboarding/LicenseNumberActivity.java
@@ -13,13 +13,21 @@ import android.widget.EditText;
 
 import org.w3c.dom.Text;
 
+import ai.elimu.appstore.BaseApplication;
 import ai.elimu.appstore.R;
+import ai.elimu.appstore.service.LicenseService;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import timber.log.Timber;
 
 public class LicenseNumberActivity extends AppCompatActivity {
 
     public static final String PREF_LICENSE_EMAIL = "pref_license_email";
     public static final String PREF_LICENSE_NUMBER = "pref_license_number";
+
+    private LicenseService licenseService;
 
     private EditText editTextLicenseEmail;
 
@@ -31,7 +39,11 @@ public class LicenseNumberActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Timber.i("onCreate");
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_license_number);
+
+        BaseApplication baseApplication = (BaseApplication) getApplication();
+        licenseService = baseApplication.getRetrofit().create(LicenseService.class);
 
         editTextLicenseEmail = findViewById(R.id.editTextLicenseEmail);
         editTextLicenseNumber = findViewById(R.id.editTextLicenseNumber);
@@ -73,10 +85,24 @@ public class LicenseNumberActivity extends AppCompatActivity {
                 String licenseEmail = editTextLicenseEmail.getText().toString();
                 String licenseNumber = editTextLicenseNumber.getText().toString();
                 if (!TextUtils.isEmpty(licenseEmail) && !TextUtils.isEmpty(licenseNumber)) {
-                    // TODO: submit to REST API for validation
-                    // TODO: if invalid license number, display error message
-                    // TODO: if valid license number, store e-mail and license number in shared preferences
-                    // TODO: if valid license number, fetch and store locale of app collection, and redirect to download activity
+                    // Submit License details to REST API for validation
+                    Call<ResponseBody> call = licenseService.getLicense(licenseEmail, licenseNumber);
+                    call.enqueue(new Callback<ResponseBody>() {
+                        @Override
+                        public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                            Timber.i("onResponse");
+
+                            // TODO: if valid license number, store e-mail and license number in shared preferences
+                            // TODO: redirect user to AppListActivity
+                        }
+
+                        @Override
+                        public void onFailure(Call<ResponseBody> call, Throwable t) {
+                            Timber.e(t, "onFailure");
+
+                            // TODO: display error message
+                        }
+                    });
                 }
             }
         });
