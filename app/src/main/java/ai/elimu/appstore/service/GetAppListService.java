d8c9479e4cff2b67dadaf407331dba63cc72412a
Tuan Nguyen <tuancoltech@gmail.com>
2017-11-25 09:51:39 +0700

    - Delete response object models with auto deserialization for application/list (app synchronization) API - Update getApplicationList API end-point declaration

--- app/src/main/java/ai/elimu/appstore/service/GetAppListService.java
+++ app/src/main/java/ai/elimu/appstore/service/GetAppListService.java
@@ -1,6 +1,6 @@
 package ai.elimu.appstore.service;
 
-import ai.elimu.appstore.model.appsynchronization.GetAppListResponse;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
@@ -11,11 +11,11 @@ import retrofit2.http.Query;
 public interface GetAppListService {
 
     @GET("application/list")
-    Call<GetAppListResponse> getApplicationList(@Query("deviceId") String deviceId,
-                                                @Query("checksum") String checkSum,
-                                                @Query("locale") String locale,
-                                                @Query("deviceModel") String deviceModel,
-                                                @Query("osVersion") int osVersion,
-                                                @Query("applicationId") String applicationId,
-                                                @Query("appVersionCode") int appVersionCode);
+    Call<ResponseBody> getApplicationList(@Query("deviceId") String deviceId,
+                                          @Query("checksum") String checkSum,
+                                          @Query("locale") String locale,
+                                          @Query("deviceModel") String deviceModel,
+                                          @Query("osVersion") int osVersion,
+                                          @Query("applicationId") String applicationId,
+                                          @Query("appVersionCode") int appVersionCode);
 }
