5a524e764a71d9b1944ca19428e7971c5fa24940
Tuan Nguyen <tuancoltech@gmail.com>
2017-12-25 23:05:13 +0700

    #135 Compare checksum after download Add utility methods to calculate MD5 hash value of a file and then check if a provided file is valid compared to an input MD5 value

--- app/src/main/java/ai/elimu/appstore/util/ChecksumHelper.java
+++ app/src/main/java/ai/elimu/appstore/util/ChecksumHelper.java
@@ -3,6 +3,11 @@ package ai.elimu.appstore.util;
 import android.content.Context;
 import android.text.TextUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 import java.security.MessageDigest;
@@ -34,5 +39,76 @@ public class ChecksumHelper {
         }
 
         return checksum;
+    }
+
+    /**
+     * Check MD5 hash value of a file if it's valid compared to provided MD5 string
+     *
+     * @param md5        Provided MD5 string
+     * @param updateFile The file whose's MD5 value needs to be checked
+     * @return true if file is valid, false if invalid
+     */
+    public static boolean checkMd5(String md5, File updateFile) {
+        if (TextUtils.isEmpty(md5) || updateFile == null) {
+            Timber.e("MD5 string empty or updateFile null");
+            return false;
+        }
+
+        String calculatedDigest = calculateMd5(updateFile);
+        if (calculatedDigest == null) {
+            Timber.e("calculatedDigest null");
+            return false;
+        }
+
+        Timber.v("Calculated digest: " + calculatedDigest);
+        Timber.v("Provided digest: " + md5);
+
+        return calculatedDigest.equalsIgnoreCase(md5);
+    }
+
+    /**
+     * Get MD5 hash value of a file
+     *
+     * @param updateFile The file whose's MD5 hash value needs to be calculated
+     * @return The MD5 hash value of input file
+     */
+    private static String calculateMd5(File updateFile) {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            Timber.e(e);
+            return null;
+        }
+
+        InputStream is;
+        try {
+            is = new FileInputStream(updateFile);
+        } catch (FileNotFoundException e) {
+            Timber.e(e);
+            return null;
+        }
+
+        byte[] buffer = new byte[8192]; //8x1024 = 8MB
+        int read;
+        try {
+            while ((read = is.read(buffer)) > 0) {
+                digest.update(buffer, 0, read);
+            }
+            byte[] md5sum = digest.digest();
+            BigInteger bigInt = new BigInteger(1, md5sum);
+            String output = bigInt.toString(16);
+            // Fill to 32 chars
+            output = String.format("%32s", output).replace(' ', '0');
+            return output;
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to process file for MD5", e);
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                Timber.e(e);
+            }
+        }
     }
 }
