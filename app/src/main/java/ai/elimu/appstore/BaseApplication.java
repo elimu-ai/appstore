5bb8996ac0d9b2874d2682fd2089830f15769f8b
Tuan Nguyen <tuancoltech@gmail.com>
2017-12-25 22:48:30 +0700

    #135 Compare checksum after download Delete existing APKs in case of current version code < 2000009

--- app/src/main/java/ai/elimu/appstore/BaseApplication.java
+++ app/src/main/java/ai/elimu/appstore/BaseApplication.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.os.Environment;
 import android.util.Base64;
 import android.util.Log;
 
@@ -13,12 +14,16 @@ import com.securepreferences.SecurePreferences;
 
 import org.greenrobot.greendao.database.Database;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import ai.elimu.appstore.dao.CustomDaoMaster;
 import ai.elimu.appstore.dao.DaoSession;
-import ai.elimu.appstore.util.AppPrefs;
+import ai.elimu.appstore.util.UserPrefsHelper;
 import ai.elimu.appstore.util.VersionHelper;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -73,6 +78,15 @@ public class BaseApplication extends Application {
         daoSession = new CustomDaoMaster(db).newSession();
 
         VersionHelper.updateAppVersion(getApplicationContext());
+
+        /**
+         * Check and delete existing APKs to avoid manually deleting corrupt files before upgrading to version 2.0.9
+         * of the Appstore application
+         */
+        int newVersionCode = VersionHelper.getAppVersionCode(context);
+        if (newVersionCode < 2000009) {
+            deleteExistingApks();
+        }
     }
 
     public DaoSession getDaoSession() {
@@ -154,5 +168,48 @@ public class BaseApplication extends Application {
             Timber.e(e);
             return "";
         }
+    }
+
+    private void deleteExistingApks() {
+
+        /**
+         * Skip checking & deleting APK files in the first start-up of application
+         */
+        if (UserPrefsHelper.getLocale(context) == null) {
+            return;
+        }
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+
+        /**
+         * Get APKs directory
+         */
+        String language = UserPrefsHelper.getLocale(context).getLanguage();
+        File apkDirectory = new File(Environment.getExternalStorageDirectory() + "/" +
+                ".elimu-ai/appstore/apks/" + language);
+
+        /**
+         * Get all APK files for deleting
+         */
+        final File[] apkFiles = apkDirectory.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String name) {
+                return (name.endsWith(".apk"));
+            }
+        });
+
+        /**
+         * Delete APK files in background thread since this operation is timely unpredictable
+         */
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                for (final File file : apkFiles) {
+                    file.delete();
+                    Timber.i("APK " + file.getAbsolutePath() + " is deleted successfully");
+                }
+            }
+        });
+
     }
 }
