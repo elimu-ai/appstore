3a1429c11b2d93cc27bb88ccd065e49bb5cc801e
Tuan Nguyen <tuancoltech@gmail.com>
2018-01-31 23:43:37 +0700

    #148 Add Retrofit service for downloading APK files Add DownloadApplicationService end point Add custom ResponseBody class which supports updating download progress Add download progress update callback interface

---
+++ app/src/main/java/ai/elimu/appstore/service/DownloadApplicationService.java
@@ -0,0 +1,17 @@
+package ai.elimu.appstore.service;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
+
+/**
+ * Retrofit integration with the REST API for downloading application file
+ */
+public interface DownloadApplicationService {
+
+    @Streaming
+    @GET
+    Call<ResponseBody> downloadApplicationFile(@Url String fileUrl);
+}
