fb2ac8a82cffefa8fe0747b141bf1ae8e8a11c27
Tuan Nguyen <tuancoltech@gmail.com>
2018-02-25 20:09:41 +0700

    #165 Fix broken offline functionality If the Internet connection is disabled and the user tries to download an application, display an error message. And do not change the button text from "Download" to "Install".

--- app/src/main/java/ai/elimu/appstore/util/ConnectivityHelper.java
+++ app/src/main/java/ai/elimu/appstore/util/ConnectivityHelper.java
@@ -32,4 +32,20 @@ public class ConnectivityHelper {
             return false;
         }
     }
+
+    /**
+     * Use this method to check the network's availability
+     *
+     * @param context The context where checking is executed
+     * @return true if network is available, false if it's not
+     */
+    public static boolean isNetworkAvailable(Context context) {
+        if (context == null) return false;
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context
+                .CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        return (networkInfo != null && networkInfo.isConnectedOrConnecting());
+    }
 }
