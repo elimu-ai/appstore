9d9ade0e90d8ed4542c5e0afe1a50acddf5953f1
Nya Elimu <nya@elimu.ai>
2020-06-11 11:00:44 +0800

    #214 Download Applications from REST API
    - Parse JSON response
    - Store Applications in Room database

--- app/src/main/java/ai/elimu/appstore/ui/applications/InitialSyncActivity.java
+++ app/src/main/java/ai/elimu/appstore/ui/applications/InitialSyncActivity.java
@@ -5,11 +5,19 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import ai.elimu.appstore.BaseApplication;
 import ai.elimu.appstore.R;
 import ai.elimu.appstore.rest.ApplicationsService;
+import ai.elimu.appstore.room.GsonToRoomConverter;
+import ai.elimu.appstore.room.RoomDb;
+import ai.elimu.appstore.room.dao.ApplicationDao;
+import ai.elimu.appstore.room.entity.Application;
 import ai.elimu.model.v2.gson.application.ApplicationGson;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -48,13 +56,61 @@ public class InitialSyncActivity extends AppCompatActivity {
             public void onResponse(Call<List<ApplicationGson>> call, Response<List<ApplicationGson>> response) {
                 Timber.i("onResponse");
 
-                // TODO
+                Timber.i("response: " + response);
+
+                // Parse the JSON response
+                Snackbar.make(textView, "Synchronizing database...", Snackbar.LENGTH_LONG).show();
+                List<ApplicationGson> applicationGsons = response.body();
+                Timber.i("applicationGsons.size(): " + applicationGsons.size());
+                if (applicationGsons.size() > 0) {
+                    processResponseBody(applicationGsons);
+                }
             }
 
             @Override
             public void onFailure(Call<List<ApplicationGson>> call, Throwable t) {
-                Timber.i("onFailure");
+                Timber.e(t, "onFailure");
 
+                Timber.e(t, "t.getCause(): " + t.getCause());
+
+                // Handle error
+                Snackbar.make(textView, t.getCause().toString(), Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void processResponseBody(List<ApplicationGson> applicationGsons) {
+        Timber.i("processResponseBody");
+
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                Timber.i("run");
+
+                RoomDb roomDb = RoomDb.getDatabase(getApplicationContext());
+                ApplicationDao applicationDao = roomDb.applicationDao();
+
+                for (ApplicationGson applicationGson : applicationGsons) {
+                    Timber.i("applicationGson.getId(): " + applicationGson.getId());
+
+                    // Check if the Application has already been stored in the database
+                    Application application = applicationDao.load(applicationGson.getId());
+                    Timber.i("application: " + application);
+                    if (application == null) {
+                        // Store the new Application in the database
+                        application = GsonToRoomConverter.getApplication(applicationGson);
+                        applicationDao.insert(application);
+                        Timber.i("Stored Application in database with ID " + application.getId());
+                    } else {
+                        // Update the existing Application in the database
+                        application = GsonToRoomConverter.getApplication(applicationGson);
+                        applicationDao.update(application);
+                        Timber.i("Updated Application in database with ID " + application.getId());
+                    }
+                }
+
+                // Redirect to the list of Applications
                 // TODO
             }
         });
