361dbbab2f09e429d365fcef0c8f646884fe35f4
Nya <info@elimu.ai>
2022-04-13 13:12:26 +0800

    chore: DevOps: Configure SSL #122

--- app/src/main/java/ai/elimu/appstore/BaseApplication.java
+++ app/src/main/java/ai/elimu/appstore/BaseApplication.java
@@ -33,11 +33,11 @@ public class BaseApplication extends Application {
     }
 
     /**
-     * E.g. "http://hin.test.elimu.ai" or "http://hin.elimu.ai"
+     * E.g. "https://hin.test.elimu.ai" or "https://hin.elimu.ai"
      */
     public String getBaseUrl() {
         Language language = SharedPreferencesHelper.getLanguage(getApplicationContext());
-        String url = "http://" + language.getIsoCode();
+        String url = "https://" + language.getIsoCode();
         if (!"release".equals(BuildConfig.BUILD_TYPE)) {
             url += ".test";
         }
