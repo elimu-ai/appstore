8bfa6ab3cf4e025700d4c17ada2ea77b79e595cd
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-31 13:54:42 +0700

    Convert MainActivity to Kotlin

--- app/src/main/java/ai/elimu/appstore/MainActivity.kt
+++ app/src/main/java/ai/elimu/appstore/MainActivity.kt
@@ -1,43 +1,39 @@
-package ai.elimu.appstore;
+package ai.elimu.appstore
 
-import android.content.Intent;
-import android.os.Bundle;
+import ai.elimu.appstore.ui.SelectLanguageActivity
+import ai.elimu.appstore.ui.applications.InitialSyncActivity
+import ai.elimu.appstore.util.SharedPreferencesHelper
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import timber.log.Timber
 
-import androidx.appcompat.app.AppCompatActivity;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Timber.i("onCreate")
+        super.onCreate(savedInstanceState)
 
-import ai.elimu.appstore.ui.SelectLanguageActivity;
-import ai.elimu.appstore.ui.applications.InitialSyncActivity;
-import ai.elimu.appstore.util.SharedPreferencesHelper;
-import ai.elimu.model.v2.enums.Language;
-import timber.log.Timber;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        Timber.i("onCreate");
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main)
     }
 
-    @Override
-    protected void onStart() {
-        Timber.i("onStart");
-        super.onStart();
+    override fun onStart() {
+        Timber.i("onStart")
+        super.onStart()
 
-        Language language = SharedPreferencesHelper.getLanguage(getApplicationContext());
-        Timber.i("language: " + language);
+        val language = SharedPreferencesHelper.getLanguage(
+            applicationContext
+        )
+        Timber.i("language: $language")
         if (language == null) {
             // Redirect to language selection
-            Intent intent = new Intent(getApplicationContext(), SelectLanguageActivity.class);
-            startActivity(intent);
-            finish();
+            val intent = Intent(applicationContext, SelectLanguageActivity::class.java)
+            startActivity(intent)
+            finish()
         } else {
             // Redirect to Activity for downloading list of Applications from REST API
-            Intent intent = new Intent(getApplicationContext(), InitialSyncActivity.class);
-            startActivity(intent);
-            finish();
+            val intent = Intent(applicationContext, InitialSyncActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
