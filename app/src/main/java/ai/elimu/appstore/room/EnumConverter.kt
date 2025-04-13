2d9c4f0d2a9c958400271b8981191a0f5741f04c
tuancoltech <tuancoltech@gmail.com>
2025-04-13 15:46:03 +0700

    Convert EnumConverter to Kotlin

--- app/src/main/java/ai/elimu/appstore/room/EnumConverter.kt
+++ app/src/main/java/ai/elimu/appstore/room/EnumConverter.kt
@@ -1,100 +1,97 @@
-package ai.elimu.appstore.room;
+package ai.elimu.appstore.room
 
-import android.text.TextUtils;
-
-import androidx.room.TypeConverter;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import ai.elimu.model.v2.enums.admin.ApplicationStatus;
-import ai.elimu.model.v2.enums.content.LiteracySkill;
-import ai.elimu.model.v2.enums.content.NumeracySkill;
-import timber.log.Timber;
+import ai.elimu.model.v2.enums.admin.ApplicationStatus
+import ai.elimu.model.v2.enums.content.LiteracySkill
+import ai.elimu.model.v2.enums.content.NumeracySkill
+import android.text.TextUtils
+import androidx.room.TypeConverter
+import org.json.JSONArray
+import org.json.JSONException
+import timber.log.Timber
 
 /**
  * See https://developer.android.com/training/data-storage/room/referencing-data
  */
-public class EnumConverter {
-
+object EnumConverter {
+    @JvmStatic
     @TypeConverter
-    public static ApplicationStatus fromApplicationStatus(String value) {
-        ApplicationStatus applicationStatus = null;
+    fun fromApplicationStatus(value: String?): ApplicationStatus? {
+        var applicationStatus: ApplicationStatus? = null
         if (!TextUtils.isEmpty(value)) {
-            applicationStatus = ApplicationStatus.valueOf(value);
+            applicationStatus = ApplicationStatus.valueOf(value!!)
         }
-        return applicationStatus;
+        return applicationStatus
     }
 
+    @JvmStatic
     @TypeConverter
-    public static String toApplicationStatus(ApplicationStatus applicationStatus) {
-        String value = null;
+    fun toApplicationStatus(applicationStatus: ApplicationStatus?): String? {
+        var value: String? = null
         if (applicationStatus != null) {
-            value = applicationStatus.toString();
+            value = applicationStatus.toString()
         }
-        return value;
+        return value
     }
 
+    @JvmStatic
     @TypeConverter
-    public static Set<LiteracySkill> fromLiteracySkills(String value) {
-        Set<LiteracySkill> literacySkills = new HashSet<>();
+    fun fromLiteracySkills(value: String?): Set<LiteracySkill> {
+        val literacySkills: MutableSet<LiteracySkill> = HashSet()
         if (!TextUtils.isEmpty(value)) {
             try {
-                JSONArray jsonArray = new JSONArray(value);
-                Timber.d("jsonArray: " + jsonArray);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    String literacySkillAsString = jsonArray.getString(i);
-                    Timber.d("literacySkillAsString: " + literacySkillAsString);
-                    LiteracySkill literacySkill = LiteracySkill.valueOf(literacySkillAsString);
-                    literacySkills.add(literacySkill);
+                val jsonArray = JSONArray(value)
+                Timber.d("jsonArray: $jsonArray")
+                for (i in 0..<jsonArray.length()) {
+                    val literacySkillAsString = jsonArray.getString(i)
+                    Timber.d("literacySkillAsString: $literacySkillAsString")
+                    val literacySkill = LiteracySkill.valueOf(literacySkillAsString)
+                    literacySkills.add(literacySkill)
                 }
-            } catch (JSONException e) {
-                Timber.e(e);
+            } catch (e: JSONException) {
+                Timber.e(e)
             }
         }
-        return literacySkills;
+        return literacySkills
     }
 
     @TypeConverter
-    public static String toLiteracySkills(Set<LiteracySkill> literacySkills) {
-        String value = null;
+    fun toLiteracySkills(literacySkills: Set<LiteracySkill?>?): String? {
+        var value: String? = null
         if (literacySkills != null) {
-            value = literacySkills.toString();
+            value = literacySkills.toString()
         }
-        Timber.i("value: " + value);
-        return value;
+        Timber.i("value: $value")
+        return value
     }
 
+    @JvmStatic
     @TypeConverter
-    public static Set<NumeracySkill> fromNumeracySkills(String value) {
-        Set<NumeracySkill> numeracySkills = new HashSet<>();
+    fun fromNumeracySkills(value: String?): Set<NumeracySkill> {
+        val numeracySkills: MutableSet<NumeracySkill> = HashSet()
         if (!TextUtils.isEmpty(value)) {
             try {
-                JSONArray jsonArray = new JSONArray(value);
-                Timber.d("jsonArray: " + jsonArray);
-                for (int i = 0; i < jsonArray.length(); i++) {
-                    String numeracySkillAsString = jsonArray.getString(i);
-                    Timber.d("numeracySkillAsString: " + numeracySkillAsString);
-                    NumeracySkill numeracySkill = NumeracySkill.valueOf(numeracySkillAsString);
-                    numeracySkills.add(numeracySkill);
+                val jsonArray = JSONArray(value)
+                Timber.d("jsonArray: $jsonArray")
+                for (i in 0..<jsonArray.length()) {
+                    val numeracySkillAsString = jsonArray.getString(i)
+                    Timber.d("numeracySkillAsString: $numeracySkillAsString")
+                    val numeracySkill = NumeracySkill.valueOf(numeracySkillAsString)
+                    numeracySkills.add(numeracySkill)
                 }
-            } catch (JSONException e) {
-                Timber.e(e);
+            } catch (e: JSONException) {
+                Timber.e(e)
             }
         }
-        return numeracySkills;
+        return numeracySkills
     }
 
     @TypeConverter
-    public static String toNumeracySkills(Set<NumeracySkill> numeracySkills) {
-        String value = null;
+    fun toNumeracySkills(numeracySkills: Set<NumeracySkill?>?): String? {
+        var value: String? = null
         if (numeracySkills != null) {
-            value = numeracySkills.toString();
+            value = numeracySkills.toString()
         }
-        Timber.i("value: " + value);
-        return value;
+        Timber.i("value: $value")
+        return value
     }
 }
