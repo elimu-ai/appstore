f85c0bf29fee240888f70b6afe0ba80f84aaf133
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-14 11:35:35 +0700

    Convert Application to Kotlin

--- app/src/main/java/ai/elimu/appstore/room/entity/Application.kt
+++ app/src/main/java/ai/elimu/appstore/room/entity/Application.kt
@@ -1,69 +1,25 @@
-package ai.elimu.appstore.room.entity;
+package ai.elimu.appstore.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
-
-import java.util.Set;
-
-import ai.elimu.model.v2.enums.admin.ApplicationStatus;
-import ai.elimu.model.v2.enums.content.LiteracySkill;
-import ai.elimu.model.v2.enums.content.NumeracySkill;
+import ai.elimu.model.v2.enums.admin.ApplicationStatus
+import ai.elimu.model.v2.enums.content.LiteracySkill
+import ai.elimu.model.v2.enums.content.NumeracySkill
+import androidx.room.Entity
 
 /**
  * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
  */
 @Entity
-public class Application extends BaseEntity {
+class Application : BaseEntity() {
+    lateinit var packageName: String
 
-    @NonNull
-    private String packageName;
+    @JvmField
+    var infrastructural: Boolean? = null
 
-    private Boolean infrastructural;
+    lateinit var applicationStatus: ApplicationStatus
 
-    @NonNull
-    private ApplicationStatus applicationStatus;
+    @JvmField
+    var literacySkills: Set<LiteracySkill>? = null
 
-    private Set<LiteracySkill> literacySkills;
-
-    private Set<NumeracySkill> numeracySkills;
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    public Boolean getInfrastructural() {
-        return infrastructural;
-    }
-
-    public void setInfrastructural(Boolean infrastructural) {
-        this.infrastructural = infrastructural;
-    }
-
-    public ApplicationStatus getApplicationStatus() {
-        return applicationStatus;
-    }
-
-    public void setApplicationStatus(ApplicationStatus applicationStatus) {
-        this.applicationStatus = applicationStatus;
-    }
-
-    public Set<LiteracySkill> getLiteracySkills() {
-        return literacySkills;
-    }
-
-    public void setLiteracySkills(Set<LiteracySkill> literacySkills) {
-        this.literacySkills = literacySkills;
-    }
-
-    public Set<NumeracySkill> getNumeracySkills() {
-        return numeracySkills;
-    }
-
-    public void setNumeracySkills(Set<NumeracySkill> numeracySkills) {
-        this.numeracySkills = numeracySkills;
-    }
+    @JvmField
+    var numeracySkills: Set<NumeracySkill>? = null
 }
