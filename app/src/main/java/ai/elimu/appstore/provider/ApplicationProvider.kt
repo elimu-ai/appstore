fca45ec3b895a08e32d8a1969b5748fc92f6da77
Tuan Nguyen <tuancoltech@gmail.com>
2025-04-16 15:12:27 +0700

    Convert ApplicationProvider to Kotlin

--- app/src/main/java/ai/elimu/appstore/provider/ApplicationProvider.kt
+++ app/src/main/java/ai/elimu/appstore/provider/ApplicationProvider.kt
@@ -1,93 +1,91 @@
-package ai.elimu.appstore.provider;
+package ai.elimu.appstore.provider
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.UriMatcher;
-import android.database.Cursor;
-import android.net.Uri;
+import ai.elimu.appstore.BuildConfig
+import ai.elimu.appstore.room.RoomDb
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.UriMatcher
+import android.database.Cursor
+import android.net.Uri
+import timber.log.Timber
 
-import ai.elimu.appstore.BuildConfig;
-import ai.elimu.appstore.room.RoomDb;
-import ai.elimu.appstore.room.dao.ApplicationDao;
-import timber.log.Timber;
+class ApplicationProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        Timber.i("onCreate")
 
-public class ApplicationProvider extends ContentProvider {
-
-    public static final String AUTHORITY = BuildConfig.APPLICATION_ID + ".provider.application_provider";
-
-    private static final String TABLE_APPLICATIONS = "applications";
-    private static final int CODE_APPLICATIONS = 1;
-
-    private static final UriMatcher MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
-
-    static {
-        MATCHER.addURI(AUTHORITY, TABLE_APPLICATIONS, CODE_APPLICATIONS);
+        return true
     }
 
-    @Override
-    public boolean onCreate() {
-        Timber.i("onCreate");
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+        Timber.i("query")
 
-        return true;
-    }
+        Timber.i("uri: $uri")
+        Timber.i("projection: $projection")
+        Timber.i("selection: $selection")
+        Timber.i("selectionArgs: $selectionArgs")
+        Timber.i("sortOrder: $sortOrder")
 
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-        Timber.i("query");
-
-        Timber.i("uri: " + uri);
-        Timber.i("projection: " + projection);
-        Timber.i("selection: " + selection);
-        Timber.i("selectionArgs: " + selectionArgs);
-        Timber.i("sortOrder: " + sortOrder);
-
-        final int code = MATCHER.match(uri);
+        val code = MATCHER.match(uri)
         if (code == CODE_APPLICATIONS) {
-            Context context = getContext();
-            if (context == null) {
-                return null;
-            }
-            RoomDb roomDb = RoomDb.getDatabase(getContext());
-            ApplicationDao applicationDao = roomDb.applicationDao();
-            Cursor cursor = applicationDao.loadAllAsCursor();
-            cursor.setNotificationUri(context.getContentResolver(), uri);
-            return cursor;
+            val context = context ?: return null
+            val roomDb = RoomDb.getDatabase(getContext())
+            val applicationDao = roomDb.applicationDao()
+            val cursor = applicationDao.loadAllAsCursor()
+            cursor.setNotificationUri(context.contentResolver, uri)
+            return cursor
         } else {
-            throw new IllegalArgumentException("Unknown URI: " + uri);
+            throw IllegalArgumentException("Unknown URI: $uri")
         }
     }
 
-    @Override
-    public String getType(Uri uri) {
-        Timber.i("getType");
+    override fun getType(uri: Uri): String? {
+        Timber.i("getType")
 
-        switch (MATCHER.match(uri)) {
-            case CODE_APPLICATIONS:
-                return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_APPLICATIONS;
-            default:
-                throw new IllegalArgumentException("Unknown URI: " + uri);
+        when (MATCHER.match(uri)) {
+            CODE_APPLICATIONS -> return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_APPLICATIONS
+            else -> throw IllegalArgumentException("Unknown URI: $uri")
         }
     }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues contentValues) {
-        Timber.i("insert");
+    override fun insert(uri: Uri, contentValues: ContentValues?): Uri? {
+        Timber.i("insert")
 
-        return null;
+        return null
     }
 
-    @Override
-    public int delete(Uri uri, String s, String[] strings) {
-        Timber.i("delete");
+    override fun delete(uri: Uri, s: String?, strings: Array<String>?): Int {
+        Timber.i("delete")
 
-        return 0;
+        return 0
     }
 
-    @Override
-    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
-        Timber.i("update");
+    override fun update(
+        uri: Uri,
+        contentValues: ContentValues?,
+        s: String?,
+        strings: Array<String>?
+    ): Int {
+        Timber.i("update")
 
-        return 0;
+        return 0
+    }
+
+    companion object {
+        const val AUTHORITY: String = BuildConfig.APPLICATION_ID + ".provider.application_provider"
+
+        private const val TABLE_APPLICATIONS = "applications"
+        private const val CODE_APPLICATIONS = 1
+
+        private val MATCHER = UriMatcher(UriMatcher.NO_MATCH)
+
+        init {
+            MATCHER.addURI(AUTHORITY, TABLE_APPLICATIONS, CODE_APPLICATIONS)
+        }
     }
 }
