7f9f3cb59aaada02e122594e162c21cbfbc6bd25
Jo G. <1451036+jo-elimu@users.noreply.github.com>
2025-04-30 18:31:44 +0700

    feat: language provider util
    #236

---
+++ utils/src/main/java/ai/elimu/appstore/utils/LanguageProviderUtil.kt
@@ -0,0 +1,14 @@
+package ai.elimu.appstore.utils
+
+import ai.elimu.model.v2.enums.Language
+import android.content.Context
+import android.util.Log
+
+object LanguageProviderUtil {
+
+    fun getLanguage(context: Context, providerId: String): Language {
+        Log.i(javaClass.name, "getLanguage")
+
+        throw NotImplementedError()
+    }
+}
