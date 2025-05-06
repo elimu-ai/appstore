24e3ad4527e2c58de062b34500d7bee66609891c
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-06 06:41:27 +0000

    Correct Javadoc reference links

--- app/src/main/java/ai/elimu/appstore/room/entity/ApplicationVersion.java
+++ app/src/main/java/ai/elimu/appstore/room/entity/ApplicationVersion.java
@@ -4,7 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.room.Entity;
 
 /**
- * For documentation, see https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model
+ * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model">model</a>
  */
 @Entity
 public class ApplicationVersion extends BaseEntity {
