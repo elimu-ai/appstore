c45f4d11c8024f3b4bcf3cc10c7701454d779e88
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-06 06:55:22 +0000

    Remove unused import

--- app/src/main/java/ai/elimu/appstore/ui/applications/ApplicationListAdapter.kt
+++ app/src/main/java/ai/elimu/appstore/ui/applications/ApplicationListAdapter.kt
@@ -17,7 +17,6 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
