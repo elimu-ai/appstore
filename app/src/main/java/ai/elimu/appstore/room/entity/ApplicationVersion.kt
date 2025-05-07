aec0b739697698cd4353f3b13ce195ef47a1eea0
Tuan Nguyen <tuancoltech@gmail.com>
2025-05-07 04:34:36 +0000

    Convert ApplicationVersion, BaseEntity to Kotlin

--- app/src/main/java/ai/elimu/appstore/room/entity/ApplicationVersion.kt
+++ app/src/main/java/ai/elimu/appstore/room/entity/ApplicationVersion.kt
@@ -1,66 +1,24 @@
-package ai.elimu.appstore.room.entity;
+package ai.elimu.appstore.room.entity
 
-import androidx.annotation.NonNull;
-import androidx.room.Entity;
+import androidx.room.Entity
 
 /**
- * For documentation, see <a href="https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model">model</a>
+ * For documentation, see [model](https://github.com/elimu-ai/webapp/tree/master/src/main/java/ai/elimu/model)
  */
 @Entity
-public class ApplicationVersion extends BaseEntity {
+class ApplicationVersion : BaseEntity() {
+    @JvmField
+    var applicationId: Long = 0
 
-    @NonNull
-    private long applicationId;
+    @JvmField
+    var fileUrl: String = ""
 
-    @NonNull
-    private String fileUrl;
+    @JvmField
+    var fileSizeInKb: Int = 0
 
-    @NonNull
-    private Integer fileSizeInKb;
+    @JvmField
+    var checksumMd5: String = ""
 
-    @NonNull
-    private String checksumMd5;
-
-    @NonNull
-    private Integer versionCode;
-
-    public long getApplicationId() {
-        return applicationId;
-    }
-
-    public void setApplicationId(long applicationId) {
-        this.applicationId = applicationId;
-    }
-
-    public String getFileUrl() {
-        return fileUrl;
-    }
-
-    public void setFileUrl(String fileUrl) {
-        this.fileUrl = fileUrl;
-    }
-
-    public Integer getFileSizeInKb() {
-        return fileSizeInKb;
-    }
-
-    public void setFileSizeInKb(Integer fileSizeInKb) {
-        this.fileSizeInKb = fileSizeInKb;
-    }
-
-    public String getChecksumMd5() {
-        return checksumMd5;
-    }
-
-    public void setChecksumMd5(String checksumMd5) {
-        this.checksumMd5 = checksumMd5;
-    }
-
-    public Integer getVersionCode() {
-        return versionCode;
-    }
-
-    public void setVersionCode(Integer versionCode) {
-        this.versionCode = versionCode;
-    }
+    @JvmField
+    var versionCode: Int = 0
 }
